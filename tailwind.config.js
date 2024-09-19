0c5709db03b29f95de03a1311db3fd292449f436
andrew101sanders <andrew101sanders@gmail.com>
2024-09-19 12:42:03 -0400

    updated tailwind to be local

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#475569',
+          light: '#94a4bB',
+          dark: '#334155',
+        },
+        secondary: {
+          DEFAULT: '#001aff',
+          light: '#BE123C',
+          dark: '#881337',
+        },
+        background: '#F8FAFC',
+        text: {
+          DEFAULT: '#1F2937',
+          light: '#94a4bB',
+        },
+      }
+    }
   },
   plugins: [],
 }
-
